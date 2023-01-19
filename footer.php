1daed51e0b6e13d56d5c7f9e0d2eefde9ec845ac
Samuel Paget <sjapaget@gmail.com>
2023-01-19 19:55:50 +0100

    Modify file structure

--- footer.php
+++ footer.php
@@ -1,19 +1,19 @@
 </div><!-- .page-content -->
-<!-- <img src="<?php echo get_stylesheet_directory_uri() ?>/images/adresse_boutique_blanc.svg" alt=""> -->
+<!-- <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/adresse_boutique_blanc.svg" alt=""> -->
 
 <!-- Page Footer -->
 <footer id="page-footer" class="<?php echo esc_attr(bard_options( 'general_footer_width' )) === 'boxed' ? 'boxed-wrapper ': ''; ?>clear-fix">
   <div class="footer__col">
     <h6 class="footer__title">Paiement sécurisé</h6>
-    <img class="footer__logo" src="<?php echo get_stylesheet_directory_uri() ?>/images/PayPal.svg" alt="PayPal Logo">
+    <img class="footer__logo" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/PayPal.svg" alt="PayPal Logo">
     <h6 class="footer__title">Livraison</h6>
-    <img class="footer__logo" src="<?php echo get_stylesheet_directory_uri() ?>/images/Colissimo.svg" alt="Colissimo Logo">
+    <img class="footer__logo" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/Colissimo.svg" alt="Colissimo Logo">
   </div>
   <div class="footer__col">
     <h6 class="footer__title">A Propos</h6>
     <a class="footer__link" href="<?php echo site_url('/soin-bijou'); ?>">Pour prendre soin de votre bijou</a>
     <h6 class="footer__title">Mes Labels</h6>
-    <img class="footer__logo-labels" src="<?php echo get_stylesheet_directory_uri() ?>/images/MesLabels.png" alt="">
+    <img class="footer__logo-labels" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/MesLabels.png" alt="">
   </div>
   <div class="footer__col">
     <h6 class="footer__title">Pour Vous Informer</h6>
@@ -25,10 +25,10 @@
     <h6 class="footer__title">Mes Reseaux Sociaux</h6>
     <div class="footer__logo--socials-container">
       <a href="https://www.facebook.com/profile.php?id=100030724736732" target="_blank">
-        <img class="footer__logo footer__logo-socials-img" src="<?php echo get_stylesheet_directory_uri() ?>/images/justine-fb.png" alt="facebook logo">
+        <img class="footer__logo footer__logo-socials-img" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/justine-fb.png" alt="facebook logo">
       </a>
       <a href="https://www.instagram.com/justinetheodor/" target="_blank">
-        <img class="footer__logo footer__logo-socials-img" src="<?php echo get_stylesheet_directory_uri() ?>/images/justine-insta.png" alt="instagram logo">
+        <img class="footer__logo footer__logo-socials-img" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/justine-insta.png" alt="instagram logo">
       </a>
     </div>
   </div>
@@ -39,7 +39,7 @@
       ou bien par la rubrique
       <a href="<?php echo site_url('/contactez-moi'); ?>">contact</a>
     </p>
-    <img class="footer__logo-address" src="<?php echo get_stylesheet_directory_uri() ?>/images/adresse_boutique_blanc.svg" alt="Social Network logos">
+    <img class="footer__logo-address" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/adresse_boutique_blanc.svg" alt="Social Network logos">
     <p class="footer__text">
       atelier boutique <strong>espace lune</strong> 34 rue de la lune 75002
     <br>
